bb00034e89cc2174354dec74d6c0a38a247fde2e
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2024-11-25 11:37:18 -0300

    add post route

--- src/server.js
+++ src/server.js
@@ -2,12 +2,27 @@ import db from './database/initializeDB.js'
 import express from 'express'
 
 const app = express()
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    const tarefas = db.prepare('SELECT * FROM tarefas').all()
-    res.status(200).json({
-        tarefas: tarefas
-    })
+app.post('/tarefas', (req, res) => {
+    const maxOrderQuery = db.prepare('SELECT MAX(ordem) AS max_ordem FROM tarefas');
+    const result = maxOrderQuery.get();
+    const ordem = result.max_ordem ? result.max_ordem + 1 : 1;
+
+    const query = db.prepare(`
+        INSERT INTO tarefas(nome, custo, data, ordem)
+        VALUES(?, ?, ?, ?)
+    `)
+
+    query.run(req.body.nome, req.body.custo, req.body.data, ordem)
+    res.status(201).json(req.body)
 })
+
+app.get('/tarefas', (req, res) => {
+    const query = db.prepare('SELECT * FROM tarefas').all()
+    res.status(200).json(query)
+})
+
+
 
 app.listen(3000)
