47872b9ff2c53c7540d00b58158f7509b3174623
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2024-11-25 12:41:08 -0300

    made put/delete routes

--- src/server.js
+++ src/server.js
@@ -15,14 +15,56 @@ app.post('/tarefas', (req, res) => {
     `)
 
     query.run(req.body.nome, req.body.custo, req.body.data, ordem)
+
     res.status(201).json(req.body)
 })
 
 app.get('/tarefas', (req, res) => {
     const query = db.prepare('SELECT * FROM tarefas').all()
+
     res.status(200).json(query)
 })
 
+app.put('/tarefas/:id', (req, res) => {
+    const query = db.prepare(`
+        UPDATE tarefas
+        SET nome = ?,
+            custo = ?,
+            data = ?
+        WHERE
+            id = ?
+    `)
 
+    query.run(req.body.nome, req.body.custo, req.body.data, req.params.id)
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/tarefas/:id', (req, res) => {
+    const tarefaId = req.params.id
+
+    const ordemQuery = db.prepare(`
+        SELECT ordem FROM tarefas WHERE id = ?
+    `);
+    const ordemTarefaDeletada = ordemQuery.get(tarefaId);
+
+    if (!ordemTarefaDeletada) {
+        return res.status(404).json({ message: "Tarefa não encontrada!" });
+    }
+
+    const delQuery = db.prepare(`
+        DELETE FROM tarefas WHERE id = ?
+    `);
+    delQuery.run(tarefaId);
+
+    const updateQuery = db.prepare(`
+        UPDATE tarefas
+        SET ordem = ordem - 1
+        WHERE ordem > ?
+    `);
+    updateQuery.run(ordemTarefaDeletada.ordem);
+
+    res.status(200).json({ message: "Tarefa deletada e lista reordenada com sucesso!" });
+})
 
 app.listen(3000)
