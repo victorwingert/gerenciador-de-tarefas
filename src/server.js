cd897f6da860016f6810228f587b348d2b8bf0fb
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2024-11-26 17:49:14 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import tarefaRoutes from './tarefaRoutes.js'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use(tarefaRoutes)
 
