cebb7013539b9bb0de72615c1498e778587b0ee7
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2024-11-29 21:11:15 -0300

    rollback

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -53,10 +53,7 @@ type id = {
 app.get("/tarefas", async () => {
 
     try {
-        const { data: tarefas } = await supabase
-            .from("tarefas")
-            .select("*")
-            .order("ordem", { ascending: true });
+        const { data: tarefas } = await supabase.from("tarefas").select("*");
 
         return { "value": tarefas };
     } catch (error) {
@@ -229,8 +226,6 @@ app.patch('/tarefas/:id', async (req, res) => {
             console.error(error);
             return res.status(500).send({ message: 'Erro ao atualizar ordem da tarefa', error });
         }
-
-        await reordenarTarefas();
 
         return res.status(200).send({ message: 'Ordem da tarefa atualizada com sucesso!', data });
     } catch (error) {
